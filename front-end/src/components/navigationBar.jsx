b6f090674c0bf518b226c99cda82e1d08da4b6e7
Bonobo King <aammiitt11001100@gmail.com>
2022-12-18 10:27:17 +0200

    fix css navbar issue

--- front-end/src/components/navigationBar.jsx
+++ front-end/src/components/navigationBar.jsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
+import Link from "next/link"
 
 const NavigationLink = ({ text, href }) => {
   return (
-    <Link htmlClass="nav-link" href={href}>
-      {text}
-    </Link>
-  );
-};
+    <li>
+      <Link href={href}>{text}</Link>
+    </li>
+  )
+}
 
-const LINKS = ["Home", "Ideas", "Projects", "About"];
+const LINKS = ["Home", "Ideas", "Projects", "About"]
 
 let navLinks = LINKS.map((link) => {
   return (
@@ -18,11 +18,11 @@ let navLinks = LINKS.map((link) => {
       href={link == "Home" ? "/" : "/" + link.toLowerCase()}
       //the Home button should redirect to "/" rather than "/home"
     />
-  );
-});
+  )
+})
 
 const NavigationBar = ({ htmlClass }) => {
-  return <div className={htmlClass}>{navLinks}</div>;
-};
+  return <nav className={htmlClass}>{navLinks}</nav>
+}
 
-export default NavigationBar;
+export default NavigationBar
