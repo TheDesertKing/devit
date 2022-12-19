bb1517cb0c06441f03deb0723269b7e6073b5f5b
Bonobo King <aammiitt11001100@gmail.com>
2022-12-19 09:28:54 +0200

    Added typescript

--- front-end/src/pages/api/db/test.js
+++ front-end/src/pages/api/db/test.js
@@ -3,11 +3,10 @@ import projectModel from "@/models/Project"
 
 export default function handler(req, res) {
   if (req.method == "GET") {
-    startMongo()
-    console.log(projectModel)
+    startMongo(true)
 
     let firstProject = new projectModel({ name: "first", id: 0 })
-    firstProject.save((err) => console.log(err))
+    firstProject.save((err) => console.log("Error: ", err))
     res.status(200).json(123)
   }
 }
