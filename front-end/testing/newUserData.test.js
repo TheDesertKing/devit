cc170394a8c7a226a7e812870d9dbb9f32ea914f
Bonobo King <aammiitt11001100@gmail.com>
2023-01-26 09:44:01 +0200

    Install supertest and setup Jest watch

--- front-end/testing/newUserData.test.js
+++ front-end/testing/newUserData.test.js
@@ -1,59 +1,61 @@
 const { parseNewUser } = require("@/api/db/user.ts")
 
-test("Creating new user with bad data", () => {
-  expect(parseNewUser({ abc: "efg" })).toBe(undefined)
-})
+describe("parseNewUser function for POST requests to /api/db/user endpoint", () => {
+  test("Parsing new user with bad data", () => {
+    expect(parseNewUser({ abc: "efg" })).toBe(undefined)
+  })
 
-test("Creating new user with no data", () => {
-  expect(parseNewUser({})).toBe(undefined)
-})
+  test("Parsing new user with no data", () => {
+    expect(parseNewUser({})).toBe(undefined)
+  })
 
-test("Creating new user with full data", () => {
-  const userData = {
-    username: "testos",
-    user_description: "testing!",
-    user_tags: ["test", "testing"],
-    github_account_link: "https://github.com/TheDesertKingabc",
-  }
-  expect(parseNewUser(userData)).toStrictEqual(userData)
-})
+  test("Parsing new user with full data", () => {
+    const userData = {
+      username: "testos",
+      user_description: "testing!",
+      user_tags: ["test", "testing"],
+      github_account_link: "https://github.com/TheDesertKingabc",
+    }
+    expect(parseNewUser(userData)).toStrictEqual(userData)
+  })
 
-test("Creating new user with partial data", () => {
-  const userData = {
-    username: "abc",
-    user_description: "description",
-  }
-  expect(parseNewUser(userData)).toStrictEqual(userData)
-})
+  test("Parsing new user with partial data", () => {
+    const userData = {
+      username: "abc",
+      user_description: "description",
+    }
+    expect(parseNewUser(userData)).toStrictEqual(userData)
+  })
 
-test("Creating new user with extra unrelated data", () => {
-  const userData = {
-    username: "abcd",
-    user_description: "descripty",
-    ididos: "uh oh",
-  }
-  const expectedData = {
-    username: "abcd",
-    user_description: "descripty",
-  }
-  expect(parseNewUser(userData)).toStrictEqual(expectedData)
-})
+  test("Parsing new user with extra unrelated data", () => {
+    const userData = {
+      username: "abcd",
+      user_description: "descripty",
+      ididos: "uh oh",
+    }
+    const expectedData = {
+      username: "abcd",
+      user_description: "descripty",
+    }
+    expect(parseNewUser(userData)).toStrictEqual(expectedData)
+  })
 
-test("Creating new user with wrong data types on optional fields", () => {
-  const userData = {
-    username: "abcd",
-    user_description: 1234,
-  }
-  expect(parseNewUser(userData)).toStrictEqual(undefined)
-})
+  test("Parsing new user with wrong data types on optional fields", () => {
+    const userData = {
+      username: "abcd",
+      user_description: 1234,
+    }
+    expect(parseNewUser(userData)).toStrictEqual(undefined)
+  })
 
-test("Creating new user with wrong data types on unrelated fields", () => {
-  const userData = {
-    username: "abcd",
-    akjakjlajkljadsfk: 1234,
-  }
-  const expectedData = {
-    username: "abcd",
-  }
-  expect(parseNewUser(userData)).toStrictEqual(expectedData)
+  test("Parsing new user with wrong data types on unrelated fields", () => {
+    const userData = {
+      username: "abcd",
+      akjakjlajkljadsfk: 1234,
+    }
+    const expectedData = {
+      username: "abcd",
+    }
+    expect(parseNewUser(userData)).toStrictEqual(expectedData)
+  })
 })
