373118aab88c34c28d8d721a98c387ebaceb3de5
TheDesertKing <aammiitt11001100@gmail.com>
2023-12-15 16:29:47 +0200

    Move idea_id out of new idea schema

--- types/zod/ideaInterface.zod.ts
+++ types/zod/ideaInterface.zod.ts
@@ -4,16 +4,16 @@ export const newIdeaSchema = z.object({
 	//required
 	idea_title: z.string().min(4).max(40),
 	submitter_id: z.string(),
-	idea_id: z.number(),
 	//optional
 	idea_description: z.string().max(140).optional(),
-	idea_tags: z.array(z.string()).max(7).optional(),
+	idea_tags: z.string().array().max(7).optional(),
 	// related_ideas: z.array(z.string()).max(4).optional(), Not Yet Implemented!
 	related_projects: z.string().array().max(4).optional(),
 	links: z.string().url().array().max(4).optional(),
 })
 
 export const ideaSchema = newIdeaSchema.extend({
+	idea_id: z.number(),
 	_id: z.string(),
 	createdAt: z.date(),
 	updatedAt: z.date()
