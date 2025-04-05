ec3e1872b2c6ea77871daf014b1f93c342eede2e
TheDesertKing <aammiitt11001100@gmail.com>
2025-04-06 01:03:55 +0300

    remove collection creationg from startMongo as they are automatically created

--- lib/startMongo.ts
+++ lib/startMongo.ts
@@ -3,10 +3,6 @@ import idea from "@/models/Idea"
 import user from "@/models/User"
 import mongoose from "mongoose"
 
-const createCollections = (models: mongoose.Model<any>[]) => {
-  user.createCollection()
-}
-
 const startMongo = async () => {
   if (!mongoose.connection.readyState) {
     mongoose.set("strictQuery", false)
@@ -14,7 +10,6 @@ const startMongo = async () => {
     await mongoose
       .connect(process.env.MONGODB_STRING)
       .catch((error) => console.log("MongoDB connection error: ", error))
-    await createCollections([user, idea, project])
   }
 
   return { user: user, idea: idea, project: project }
